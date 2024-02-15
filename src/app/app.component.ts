177be9e2b58ee1fc6e6735f3c9a5b9fcbea54889
Darshan Sakpal <d.sakpal38@gmail.com>
2024-02-15 12:57:01 +0000

    Added sorting logic to sort the data based on Firstnam, Lastname, oldest and latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,16 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
-import { SocketIoService } from './socket.io.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit {
   title = 'golf-tournament-ui';
 
-  constructor(private socketService: SocketIoService) { }
+  constructor() { }
 
   ngOnInit() {
-    // this.socketService.getPlayersData();
-    // console.log(playersData)
-  }
-
-  ngOnChanges() {
-    // this.socketService.getPlayersData();
-    // console.log(this.socketService.playersData)
   }
 }
--- src/app/player-data/player-data.component.ts
+++ src/app/player-data/player-data.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SocketIoService } from '../socket.io.service';
 import { io } from 'socket.io-client';
 
 @Component({
@@ -11,9 +10,10 @@ export class PlayerDataComponent implements OnInit {
   socket: any;
   url: string = 'https://mst-full-stack-dev-test.herokuapp.com/';
   playersData: any[] = [];
+  reversedplayersData: any[] = [];
   displayData: any;
   visible: Boolean = false;
-  constructor(private socketService: SocketIoService) {
+  constructor() {
     this.socket = io(this.url)
   }
 
@@ -22,24 +22,43 @@ export class PlayerDataComponent implements OnInit {
   }
 
   getPlayersData() {
+    // fetches data from the web socket url
     this.socket.on('data-update', (data: object) => {
       console.log(data);
       this.playersData.push(data);
       this.playersData.reverse();
     })
-    // return this.playersData;
   }
 
   displayHoleScore(mst_id: any) {
+    // Matches the MSTID in the array and shows the data corresponding to that MSTID
     const found = this.playersData.find((obj) => {
       return obj.MSTID === mst_id;
     });
 
     if (found) {
-      console.log("Match found")
       this.displayData = found;
       console.log(this.displayData)
       this.visible = !this.visible;
     }
+  }
+
+  showLatestData() {
+    // shows the latest data first
+    this.playersData.reverse();
+  }
+
+  showOldestData() {
+    // shows the oldest data first
+    this.playersData.reverse();
+  }
+  sortByFirstName() {
+    // sorts the array on the basis of firstname
+    this.playersData.sort((a, b) => a.First.toUpperCase().localeCompare(b.First.toUpperCase()));
+  }
+
+  sortByLastName() {
+    // Sorts the array on the basis of lastname
+    this.playersData.sort((a, b) => a.Last.toUpperCase().localeCompare(b.Last.toUpperCase()));
   }
 }
